95614f7d659946402524035e1ac9e0c592b459bf
kevin kennedy <kjkennedy96@gmail.com>
2023-02-02 21:36:43 +0000

    Completed Lab Unchecked Exceptions

--- src/main/java/CauseUncheckedException.java
+++ src/main/java/CauseUncheckedException.java
@@ -14,6 +14,8 @@ public class CauseUncheckedException {
      * event happens, like running out of computer memory.
      */
     public void throwUncheckedException(){
-
+        int num = 0;
+        int num2 = 15876;
+        int sum = num2/num;
     }
 }
